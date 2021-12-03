5c212ec9a93172a3d11d8abf36f9005c3eb5fa21
Joosep-P <minu.git@mailnull.com>
2021-12-03 12:23:56 +0200

    asd

--- restapi/src/main/java/com/example/restapi/models/Menu.java
+++ restapi/src/main/java/com/example/restapi/models/Menu.java
@@ -7,8 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
