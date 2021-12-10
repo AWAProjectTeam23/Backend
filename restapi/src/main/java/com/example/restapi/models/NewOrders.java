06bd03c370a022f0e7c181eae7cee8959c83cfee
antero.j.korvala@gmail.com <antero.j.korvala@gmail.com>
2021-12-10 12:22:49 +0200

    Updated ShoppingCar#2

--- restapi/src/main/java/com/example/restapi/models/NewOrders.java
+++ restapi/src/main/java/com/example/restapi/models/NewOrders.java
@@ -4,23 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NewOrders {
-    private final String customer_uuid;
     private final String restaurant_uuid;
     private final String totalPrice;
     private final String delivery_location;
     public List<OrderProducts> orderProducts;
 
-    public NewOrders(String customer_uuid, String restaurant_uuid, String totalPrice,
+    public NewOrders(String restaurant_uuid, String totalPrice,
                      List<OrderProducts> orderProducts, String delivery_location) {
-        this.customer_uuid = customer_uuid;
         this.restaurant_uuid = restaurant_uuid;
         this.totalPrice = totalPrice;
         this.orderProducts = orderProducts;
         this.delivery_location = delivery_location;
-    }
-
-    public String getCustomer_uuid() {
-        return customer_uuid;
     }
 
     public String getRestaurant_uuid() {
